925ac5f87d146a13391970c675031f72e693b2ef
[dylanjwu] <dylanjwu@gmail.com>
2021-02-18 16:55:18 -0500

    added LRU eviction policy, and general algo; fails to run

--- myhub.py
+++ myhub.py
@@ -1,13 +1,34 @@
 # the only import statement you should ever need
 # gives you access to Switchyard classes/functions
 from switchyard.lib.userlib import *
+import time
+
+f_table = {}
+
+def find_first_open_port():
+    for port in f_table:
+        if port['addr'] == None:
+            return port
+    return None
+
+def find_port_to_evict(net):
+    oldest_time = f_table[net.ports()]['time']
+    oldest_port = net.ports()[0]
+    for port in f_table:
+        if port['time'] < oldest_time:
+            oldest_time = port['time']
+            oldest_port = port
+    return oldest_port
 
 
 # net: network object; is necessary
 def main(net):
+
     log_info("Hub is starting up with these ports")
     for port in net.ports():
         log_info("{}: ethernet address {}".format(port.name, port.ethaddr))
+        f_table[port.name] = {'addr': None, 'time': None}
+        print(f_table)
 
     while True:
         try:
@@ -16,8 +37,19 @@ def main(net):
             # raises shutdown exception if Switchyard framework is shut down
             # raises NoPackets exception if no packets received before timeout value
             timestamp,input_port,packet = net.recv_packet()
-            if packet[0].dst in [p.ethaddr for p in net.ports()]:
-                continue
+            if f_table[input_port]['addr'] == None:
+                f_table[input_port]['addr'] = packet[0].src
+                f_table[input_port]['time'] = timestamp
+
+            open_port = find_first_open_port()
+            if open_port != None:
+                f_table[open_port]['addr'] = packet[0].src
+                f_table[open_port]['time'] = timestamp
+
+            else:
+                to_evict = find_port_to_evict(net) 
+                f_table[to_evict]['addr'] = packet[0].src
+                f_table[to_evict]['time'] = timestamp
 
             """
             alternatively:
@@ -43,14 +75,25 @@ def main(net):
         # assigned to one of the ports in the hub
         # then the frame should be ignored
 
+        # check if port has same address as the packet's dst address
+        already_sent = False
+        for port in f_table.keys():
+            if port != input_port and port['addr'] == packet[0].dst:
+                net.send_packet(port, packet)
+                already_sent = True
+
 
         # send packet out all ports accept the one from which it arrived
-        for port in net.ports():
-            if port.name != input_port:
-                net.send_packet(port.name, packet)
+        if not already_sent:
+            for port in net.ports():
+                if port.name != input_port:
+                    net.send_packet(port.name, packet)
 
     # should always be the last thing to do
     net.shutdown()
+
+
+
 
 
 # TODO: Implement learning bridge algorithm 
